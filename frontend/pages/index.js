f9e25509f81abe3209c9c7801b62da95a52050e6
Emily Baumgartner <emily.r.baum@gmail.com>
2019-11-24 15:26:46 -0500

    first commit

---
+++ frontend/pages/index.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+const Home = props => (
+    <div>
+        <p>
+            Home page
+        </p>
+    </div>
+)
+
+export default Home; 
---
+++ frontend/pages/sell.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+const Sell = props => (
+    <div>
+        <p>
+            sell it!
+        </p>
+    </div>
+)
+
+export default Sell; 
