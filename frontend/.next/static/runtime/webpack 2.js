e6de394d56452c9e05236f13686e51728deb1a80
Emily Baumgartner <emily.r.baum@gmail.com>
2019-12-11 23:32:07 -0500

    items are adjusting as you advance through

--- frontend/.next/static/runtime/webpack 2.js
+++ frontend/.next/static/runtime/webpack 2.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "eda94954a9be782f58ab";
+/******/ 	var hotCurrentHash = "a4d1b947d50f9a194c70";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
