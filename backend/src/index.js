4fc338f3c1d25ff6c1b62f88767a1e0a22cfdbef
Emily Baumgartner <emily.r.baum@gmail.com>
2019-12-14 22:08:58 -0500

    added user mutation on backend and connected to prisma

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,14 @@
+const cookieParser = require('cookie-parser');
+
 require('dotenv').config({ path: 'variables.env'});
 const createServer = require('./createServer');
 const db = require('./db');
 
 const server = createServer();
 
-// TODO user express middleware to handle cookies (JWT)
+// use express middleware to handle cookies (JWT)
+server.express.use(cookieParser());
+
 // TODO use express middleware to populate current user
 
 server.start(
--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const Mutations = {
     async createItem(parent, args, ctx, info) {
         // TODO: check if they are logged in
@@ -30,6 +32,31 @@ const Mutations = {
             // TODO
         // delete it!
         return ctx.db.mutation.deleteItem({ where }, info);
+    },
+    async signup(parent, args, ctx, info) {
+        // lowercase their email
+        args.email = args.email.toLowerCase();
+        // hash their password
+        const password = await bcrypt.hash(args.password, 10);
+        // create the user in the database
+        const user = await ctx.db.mutation.createUser(
+        {
+            data: {
+                ...args,
+                password,
+                permissions: { set: ['USER'] },
+            },
+        }, 
+        info);
+        // create the JWT token for them to be signed in after signing up
+        const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET)
+        // set the jwt as a cookie on the response
+        ctx.response.cookie('token', token, {
+            httpOnly: 'true',
+            maxAge: 1000 * 60 * 60 * 24 * 365 // 1 year cookie
+        });
+        // then we retun the user to the browser
+        return user;
     }
 };
 
