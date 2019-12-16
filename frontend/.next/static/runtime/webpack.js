427e936341e18845cd1b1e39677ee9a78e9da846
Emily Baumgartner <emily.r.baum@gmail.com>
2019-12-15 23:47:34 -0500

    added user sign up page and component

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "eda94954a9be782f58ab";
+/******/ 	var hotCurrentHash = "c987b8fec882bdffda52";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
