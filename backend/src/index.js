754b68bbe8ca94e73cdc54a93df7903ec0cb2224
Emily Baumgartner <emily.r.baum@gmail.com>
2019-12-18 00:13:10 -0500

    added ability to get the cookie from the browser and check if user is signed in and set up user query to be reusable as User

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,5 @@
 const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 
 require('dotenv').config({ path: 'variables.env'});
 const createServer = require('./createServer');
@@ -9,8 +10,16 @@ const server = createServer();
 // use express middleware to handle cookies (JWT)
 server.express.use(cookieParser());
 
-// TODO use express middleware to populate current user
-
+// decode the jwt so we can get the user ID on each request 
+server.express.use((req, res, next) => {
+    const{ token } = req.cookies;
+    if (token) {
+        const { userId } = jwt.verify(token, process.env.APP_SECRET)
+        // put userId onto the req for future requests 
+        req.userId = userId 
+    }
+    next();
+})
 server.start(
     {
         cors: {
